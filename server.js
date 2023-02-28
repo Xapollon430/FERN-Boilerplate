bd3f03292367da538a5afca0155afe21d9a77100
DanOukolov <oukolovdaniel@gmail.com>
2023-02-28 07:53:14 -0500

    Redo Schema

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(port, () => {
